66d674836d97bd70fce0d077fed5f40198a71bc6
nikikachelmaier <niki.kachelmaier@gmail.com>
2023-03-18 10:34:33 +0100

    deployment script

---
+++ deploy_rg.sh
@@ -0,0 +1,17 @@
+# Azure Batch Script for deploying a ARM sample Template
+
+#Login and set subcription tenant
+echo Login for gid subscription
+az login
+
+# Create example Resource Group
+echo "Create Resource Group for testing"
+az group create -l northeurope -n ExampleGroup
+
+# Deploy the ARM Template
+echo "Deploy the ARM Template"
+az deployment group create \
+  --name ExampleDeployment \
+  --resource-group ExampleGroup \
+  --template-uri https://raw.githubusercontent.com/nik879/azterac/main/template.json \
+  --parameters parameters.json \
